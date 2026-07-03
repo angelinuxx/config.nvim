053ee26d30d96f0316037694eb02d539c94fe5da
angelinuxx <angelinuxx@gmail.com>
2026-07-03 22:29:17 +0200

    feat: override dadbod default config and add plugin for ssh tunneling

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,23 +1,25 @@
--- WARN: Installing the LazyExtra for sql language, the following plugin is already included
 return {
-  -- "kristijanhusak/vim-dadbod-ui",
-  -- dependencies = {
-  --   { "tpope/vim-dadbod" },
-  --   { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } },
-  -- },
-  -- cmd = {
-  --   "DBUI",
-  --   "DBUIToggle",
-  --   "DBUIAddConnection",
-  --   "DBUIFindBuffer",
-  -- },
-  -- keys = {
-  --   { "<leader>D", "<cmd>DBUIToggle<cr>", desc = "Toggle DB UI" },
-  -- },
-  -- init = function()
-  --   -- Your DBUI configuration
-  --   vim.g.db_ui_use_nerd_fonts = 1
-  --   vim.g.db_ui_show_database_icon = 1
-  --   vim.g.db_ui_execute_on_save = 0
-  -- end,
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      -- Enable connection via ssh tunnel
+      "pbogut/vim-dadbod-ssh",
+    },
+    init = function()
+      local data_path = vim.fn.stdpath "data"
+
+      -- Custom save location
+      vim.g.db_ui_save_location = vim.fn.stdpath "config" .. "/dadbod_connections"
+
+      -- ==========================================
+      -- LazyVim defaults
+      -- ==========================================
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+    end,
+  },
 }
